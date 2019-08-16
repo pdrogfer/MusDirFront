8e279549ab24edb200a29fcb9a3c95237d6be50c
Pedro Gonzalez <pdro74@hotmail.com>
2019-08-16 10:32:59 +0200

    add placeholder image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GigListComponent } from './gig-list/gig-list.component';
+import { GigDetailComponent } from './gig-detail/gig-detail.component';
+import { BandDetailComponent } from './band-detail/band-detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GigListComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
